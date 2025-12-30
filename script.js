3f441a5bf521f30390c1a6136daec88d1bca55f2
pachotadas27-gif <pachotadas27@gmail.com>
2025-12-30 12:59:23 -0500

    Fix Rimac camera URL

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const cameraData = [
   { slug: "ejercito", name: "Av. Del Ejército", location: "Magdalena", m3u8: "https://live.smartechlatam.online/claro/ejercito/index.m3u8" },
   { slug: "jesusmaria", name: "Av. Faustino Sanchez", location: "Jesús María", m3u8: "https://live.smartechlatam.online/claro/avfaustinocarrion/index.m3u8" },
   { slug: "lamarina", name: "Av. La Marina", location: "Pueblo Libre", m3u8: "https://live.smartechlatam.online/claro/lamarina/index.m3u8" },
-  { slug: "rimac", name: "Av. Prol. Tacna", location: "Rímac", m3u8: "https://live.smartechlatam.online/claro/rimac/index.m3u8" },
+  { slug: "rimac", name: "Av. Prol. Tacna", location: "Rímac", m3u8: "https://live.smartechlatam.online/claro/prolongaciontacna/index.m3u8" },
   { slug: "kennedy", name: "Parque Kennedy", location: "Miraflores", m3u8: "https://hd-auth.skylinewebcams.com/live.m3u8?a=09q0osufd75jb9tgr9lpg3da32" }
 ];
 
